cb2a4e5d94b7a11e88f0b779080b0384b46703b5
tduyng <tduyng.dev@gmail.com>
2024-09-20 20:57:24 +0200

    feat: match positive chars group

--- src/lib.rs
+++ src/lib.rs
@@ -11,11 +11,23 @@ pub fn run(config: cli::Config) -> Result<(), Box<dyn std::error::Error>> {
     }
 }
 
-fn match_pattern(input: &str, pattern: &str) -> bool {
-    match pattern {
-        _ if pattern.chars().count() == 1 => input.contains(pattern),
-        "\\d" => input.chars().any(|c| c.is_digit(10)),
-        "\\w" => input.chars().any(|c| c.is_alphanumeric() || c == '_'),
-        _ => unimplemented!("Pattern '{}' is not implemented yet.", pattern),
+pub fn match_pattern(input: &str, pattern: &str) -> bool {
+    if is_char_group(pattern) {
+        let chars: Vec<char> = pattern[1..pattern.len() - 1].chars().collect();
+        input.chars().any(|c| chars.contains(&c))
+    } else {
+        match pattern {
+            _ if pattern.chars().count() == 1 => input.contains(pattern),
+            "\\d" => input.chars().any(|c| c.is_digit(10)),
+            "\\w" => input.chars().any(|c| c.is_alphanumeric() || c == '_'),
+            _ => {
+                eprintln!("Unhandled pattern: {}", pattern);
+                false
+            }
+        }
     }
+}
+
+fn is_char_group(pattern: &str) -> bool {
+    pattern.starts_with('[') && pattern.ends_with(']')
 }
