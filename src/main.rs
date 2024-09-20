58d2bc4c52d558f7c0ffd2a9da9250f458afe103
tduyng <tduyng.dev@gmail.com>
2024-09-20 20:23:08 +0200

    feat: match literal character

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,6 @@ use std::env;
 use std::io;
 use std::process;
 
-fn match_pattern(input_line: &str, pattern: &str) -> bool {
-    if pattern.chars().count() == 1 {
-        return input_line.contains(pattern);
-    } else {
-        panic!("Unhandled pattern: {}", pattern)
-    }
-}
-
 // Usage: echo <input_text> | your_grep.sh -E <pattern>
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -25,10 +17,17 @@ fn main() {
 
     io::stdin().read_line(&mut input_line).unwrap();
 
-    // Uncomment this block to pass the first stage
-    // if match_pattern(&input_line, &pattern) {
-    //     process::exit(0)
-    // } else {
-    //     process::exit(1)
-    // }
+    if match_pattern(&input_line, &pattern) {
+        process::exit(0)
+    } else {
+        process::exit(1)
+    }
+}
+
+fn match_pattern(input_line: &str, pattern: &str) -> bool {
+    if pattern.chars().count() == 1 {
+        return input_line.contains(pattern);
+    } else {
+        panic!("Unhandled pattern: {}", pattern)
+    }
 }
